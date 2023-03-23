c6c38a6b3b06e1d3fbf984a3ff18768502f80461
Lukas <lukj@itu.dk>
2023-03-23 12:19:40 +0100

    And, Or and not done

--- src/LTL.hs
+++ src/LTL.hs
@@ -2,8 +2,10 @@
 {-# LANGUAGE GADTs #-}
 module LTL where
 import Rattus.Stream hiding (const)
+import qualified Rattus.Stream as RS
 import Evaluators
 import Helpers
+import Rattus
 
 type StrPred a = Str a -> Str Bool
 
@@ -11,22 +13,26 @@ data TPred a where
     SP  :: StrPred a -> TPred a
     Not :: TPred a -> TPred a
     And :: TPred a -> TPred a -> TPred a
+    Or  :: TPred a -> TPred a -> TPred a
+
     -- etc.
 
 evalLTL' :: TPred a -> Str a -> Str Bool
 evalLTL' formulae aStr =
     case formulae of
-        SP aStrPred ->  aStrPred aStr
-        Not aTPred -> negateStr $ evalLTL' aTPred aStr
-        -- Not (SP spf) -> evalLTL (SP (negateStr . spf)) aStr
-        -- Not (Not aStrPred)  -> evalLTL aStrPred aStr
-        -- Not tpF -> evalLTL tpF 
-        And _ _ -> constStr True
+        SP aStrPred -> aStrPred aStr
+        Not aTPred  -> negateStr $ evalLTL' aTPred aStr
+        And phi psi -> RS.zipWith (box (&&)) (evalLTL' phi aStr) (evalLTL' psi aStr)
+        Or phi psi  -> RS.zipWith (box (||)) (evalLTL' phi aStr) (evalLTL' psi aStr)
+
     -- etc.
+
 
 evalLTL :: TPred a -> Str a -> Bool
 evalLTL formulae aStr = allTrue  (evalLTL'  formulae aStr)
 
+tautology :: TPred a
 tautology = SP (\_ -> constStr True)
-
+mkSP :: Str Bool -> TPred a
+mkSP aStr =   SP (const aStr)
 
--- test/LTLTest.hs
+++ test/LTLTest.hs
@@ -5,8 +5,8 @@ module LTLTest (runTests) where
 import Generators hiding (next)
 import Helpers
 import Test.QuickCheck
-import Rattus.Stream
-import qualified Rattus.Stream as RS
+import Rattus.Stream hiding (const)
+import qualified Rattus.Stream as RS hiding (const)
 import Rattus
 import qualified Data.Set as Set
 import Helpers
@@ -15,34 +15,59 @@ import LTL
 
 -- evalLTL tests ----------------------------------------------------------
 prop_SPConstTrue :: Bool
-prop_SPConstTrue = 
-    let expr5 = SP (\_ -> constStr True) in 
+prop_SPConstTrue =
+    let expr5 = SP (\_ -> constStr True) in
     evalLTL expr5 (constStr True) && evalLTL expr5 (constStr False)
 
 prop_SPConstTrue_AnyIn :: Str String -> Bool
-prop_SPConstTrue_AnyIn aStr = 
-    let expr5 = SP (\_ -> constStr True) in 
+prop_SPConstTrue_AnyIn aStr =
+    let expr5 = SP (\_ -> constStr True) in
     evalLTL expr5 aStr
 
 prop_NotAllFalse :: Str String -> Bool
-prop_NotAllFalse aStr = 
+prop_NotAllFalse aStr =
     let nExpr = Not (SP (\_ -> constStr False)) in
     evalLTL nExpr aStr
 
 prop_NotContractionTrue :: Bool
-prop_NotContractionTrue = 
+prop_NotContractionTrue =
     evalLTL (Not contradiction) aStr
-    where 
-        contradiction = SP(RS.map (box (>10)))
-        aStr = constStr (0::Int) 
+    where
+        contradiction = SP (RS.map (box (>10)))
+        aStr = constStr (0::Int)
 
 prop_NotIsInverse :: Str Bool -> Bool
 prop_NotIsInverse _aStr =
     evalLTL (Not (Not tautology)) _aStr
 
+-- ~(~phi ^ phi) == true.
+prop_AndContradictionAndTautologyContradiction :: Str Bool-> Bool
+prop_AndContradictionAndTautologyContradiction anyStr =
+    evalLTL (Not 
+                (And 
+                    (Not (mkSP anyStr)) 
+                    (mkSP anyStr)
+                )
+            ) 
+            anyStr
 
-    
+-- ~phi || phi == true.
+prop_OrContradictionAndTautologyContradiction :: Str Bool -> Bool
+prop_OrContradictionAndTautologyContradiction anyStr =
+    evalLTL (Or 
+                (Not (mkSP anyStr)) 
+                (mkSP anyStr)
+            ) 
+            anyStr 
 
+-- phi || T
+prop_OrTIsNeutral :: Str Bool -> Bool
+prop_OrTIsNeutral anyStr = 
+    evalLTL (Or 
+                (mkSP anyStr)
+                tautology
+            ) 
+            anyStr
 
 return []
 runTests :: IO Bool
