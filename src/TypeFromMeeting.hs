9fc680ca4f3808d70c24ad325f5db06fb1650c44
Lukas <lukj@itu.dk>
2023-04-24 08:54:43 +0200

    Added new types for feedback

--- src/TypeFromMeeting.hs
+++ src/TypeFromMeeting.hs
@@ -1,24 +1,34 @@
-module TypesFromMeetings where
+module TypeFromMeetings where
+import Helpers 
+import Test.QuickCheck
+import LTL
 --  0
 newtype Stamate a = Next (a -> Either Bool (Stamate a)) 
--- Left: check is done with passing is  true false, Right: more checking is needed.
-newtype Stamage a = Next (Gen (Maybe (a, Stamage a)))
--- Nothing means it did not succeed
+-- Left: check is done when passing is true, false it it failed, Right: more checking is needed.
 
--- make StrPred a:: a -> Bool 
+-- More expressive type:
+newtype Stamate' a = NextT (a -> Checker a)
+data Checker a = Pass 
+                | Fail 
+                | Stamate' a 
+
+newtype Stamage a = NextG (Gen (Maybe (a, Stamage a)))
+-- Nothing means it did not succeed in generating suitable.
+
+type StrPred a = a -> Bool 
+
 -- 1 
 mkStamage :: Stamate a -> Stamage a
 mkStamage _ = errorNotImplemented
 
-mkStamate :: Int -> TPred a -> Stamate a
-mkStamate n p = 
+mkStamate :: TPred a -> Stamate a
+mkStamate _ = errorNotImplemented
 
 -- 2
 suchThat :: Stamage a -> Stamate a -> Stamage a
 suchThat _ _ = errorNotImplemented
 
--- 3 Implement back tracking in 2
-
+-- 3 Implement back tracking in v2
 and :: Stamate a -> Stamate a -> Stamate a
 and _ _ = errorNotImplemented
  
