5149c309f514cf73b79d585be05acc95b52903ab
Lukas <lukj@itu.dk>
2023-05-02 15:06:59 +0200

    Renaming of core Done

---
+++ src/CoreRn.hs
@@ -0,0 +1,124 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE GADTs #-}
+module CoreRn where 
+import Rattus.Stream (Str(..))
+import Rattus.Primitives (delay, adv)
+import Test.QuickCheck (Gen, arbitrary, Arbitrary, resize)
+import Data.Maybe (fromJust)
+
+--- Types ----------------------------------------------------------------------------
+data TPred a where
+    Atom        :: (a -> Bool) -> TPred a
+    Not         :: TPred a -> TPred a
+    Or          :: TPred a -> TPred a -> TPred a
+    Until       :: TPred a -> TPred a -> TPred a
+    Imminently  :: TPred a -> TPred a
+    And         :: TPred a -> TPred a -> TPred a
+    Implies     :: TPred a -> TPred a -> TPred a
+    Always      :: TPred a -> TPred a
+    Eventually  :: TPred a -> TPred a
+    After       :: Int -> TPred a -> TPred a
+
+data Acceptor a = Accept
+                | Reject
+                | NextA (a -> Acceptor a)
+
+instance Show (Acceptor a) where
+    show Accept     = "P"
+    show Reject     = "F" 
+    show (NextA _)  = "N"
+
+newtype Transducer a = NextT (Gen (Maybe (a, Transducer a)))
+
+instance Show a => Show (Transducer a) where
+  show _ = "a Transducer" 
+
+--- Runners -----------------------------------------------------------------------
+trans :: Transducer a -> Gen (Str a) 
+trans (NextT aGen) = do
+    aMaybe <- aGen
+    let (value, aTransducer) = fromJust aMaybe 
+    rest <- trans aTransducer
+    return $ value ::: delay rest
+
+accept :: Str a -> Acceptor a -> Bool
+accept aStr anAcceptor  = accept' aStr anAcceptor 20
+
+accept' :: Str a -> Acceptor a -> Int ->  Bool
+accept' _ Accept _ = True
+accept' _ Reject _ = False
+accept' (h ::: t) (NextA makeNext) checksLeft =
+    checksLeft == 0 
+    || case makeNext h of
+            Accept -> True
+            Reject -> False
+            continuation -> accept' (adv t) continuation (checksLeft - 1)
+        
+
+--- Makers --------------------------------------------------------------------------
+rejectTransducer :: Transducer a
+rejectTransducer = NextT (return Nothing)
+
+arbitraryTransducer :: forall a. (Arbitrary a) => Transducer a
+arbitraryTransducer = NextT $ do
+    element <- (arbitrary :: Gen a)
+    return (Just (element, arbitraryTransducer))
+
+mkTransducer :: (Arbitrary a) => Acceptor a -> Transducer a
+mkTransducer anAcceptor = arbitraryTransducer `restrictWith` anAcceptor
+
+mkAcceptor :: TPred a -> Acceptor a
+mkAcceptor formulae =
+    case formulae of
+            Atom headPred   -> NextA (\h -> if headPred h then Accept else Reject)
+            Not phi         -> negateA $ mkAcceptor phi
+            Or phi psi      -> mkAcceptor (Not (Not phi `And` Not psi)) 
+            And phi psi     -> mkAcceptor phi `andA` mkAcceptor psi
+            Implies phi psi -> mkAcceptor (Not phi `Or` psi)
+            Imminently phi  -> NextA (\_ -> mkAcceptor phi)
+            Eventually phi  -> mkAcceptor $ phi `Or` Imminently (Eventually phi)
+            Until phi psi   -> mkAcceptor $ psi `Or` Imminently (phi `Until` psi)
+            Always phi      -> mkAcceptor $ phi `And` Imminently (Always phi)
+            After anInt phi -> mkAcceptor $ if anInt == 0
+                                            then phi
+                                            else Imminently (After (anInt - 1) phi)
+
+--- Modifiers ------------------------------------------------------------------------
+negateA :: Acceptor a -> Acceptor a
+negateA Reject = Accept
+negateA Accept = Reject
+negateA (NextA f) =  NextA (negateA . f)
+
+
+andA :: Acceptor a -> Acceptor a -> Acceptor a
+andA (NextA f1) (NextA f2) =
+    NextA $ \x1 ->
+        -- trace ("debug:"++ show (NextA f1)) $
+        case f1 x1 of
+            Accept -> NextA f2
+            Reject -> Reject
+            NextA f1Inner -> NextA f1Inner `andA` f2 x1
+
+andA Reject _ = Reject
+andA _ Reject = Reject
+andA Accept Accept = Accept
+andA Accept st1 = st1
+andA st2 Accept = st2
+
+restrictWith :: Transducer a -> Acceptor a -> Transducer a
+restrictWith _ Reject = rejectTransducer
+restrictWith aTransducer Accept = aTransducer
+restrictWith (NextT gen) (NextA passTest) =
+    let nTries = 1000
+        sizeSuggest = 10
+        loop n = do
+            value <- resize sizeSuggest gen
+            case value of
+                Nothing -> return Nothing
+                Just (genVal, nextGen) ->
+                    -- trace ("genVal: "++ show genVal) $ 
+                    case passTest genVal of
+                        Accept -> return $ Just (genVal, nextGen)
+                        Reject -> if n == 0 then return Nothing else loop (n-1)
+                        anAcceptor -> return $ Just (genVal, restrictWith nextGen anAcceptor)
+    in NextT $ loop (nTries::Int)
